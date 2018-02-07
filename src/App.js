d5a3e044f135f631a9afa1b8d5a1888b5526ad9c
Mayank Agarwal <mayank3242@gmail.com>
2018-02-08 00:40:38 +0530

    067 Adding Pseudo Selectors

--- src/App.js
+++ src/App.js
@@ -57,16 +57,8 @@ class App extends Component {
   }
 
   render() {
-    const style = {
-      backgroundColor: 'green',
-      color: 'white',
-      font: 'inherit',
-      border: '1px solid blue',
-      padding: '8px',
-      cursor: 'pointer'
-    };
-
     let persons = null;
+    let btnClass = '';
 
     if (this.state.showPersons) {
       persons = (
@@ -82,7 +74,7 @@ class App extends Component {
             })}
         </div>
       );
-      style.backgroundColor = 'red';
+      btnClass = Styles.Red;
     }
     const assignClass = [Styles.red];  // classes = ['red']
     if (this.state.persons.length <= 2) {
@@ -96,9 +88,8 @@ class App extends Component {
         <h1>Hi, I'm a React App</h1>
         <p className={assignClass.join(' ')}>This is really working!</p>
         <button
-          style={style}
+        className = {btnClass}
           onClick={this.togglePersonHandler}>Toggle Persons</button>
-
         {persons}
       </div>
     );
